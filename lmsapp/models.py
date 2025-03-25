10e7a1ea4ddb575b239c7d9c103e53f5b1e58513
ayush118 <ayush@cleverground.com>
2025-03-25 16:20:23 +0530

    Commit2

--- lmsapp/models.py
+++ lmsapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# role = True is admin else normal User
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    role = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+# class Admin(models.Model):
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=255)
+
+#     def __str__(self):
+#         return self.email
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.title
